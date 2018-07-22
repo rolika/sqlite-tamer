b08990898d1bbd7670cfbda3a8237972a784f072
Weisz Roland <weisz.roland@wevik.hu>
2018-07-22 16:08:07 +0200

    rewritten as object

--- tamer.py
+++ tamer.py
@@ -1,15 +1,5 @@
 """
-sqlite3_tamer module
-Functions for simple database handling:
-init():     initialize a database connection
-create():   create table
-insert():   insert row
-select():   select row(s)
-delete():   delete row(s)
-update():   update row
-destroy():  drop database file
-TODO drop():   delete table
-TODO execute(): execute a user provided SQL-statement
+Module for simple data persistence using the SQLite architecture
 """
 
 
@@ -18,202 +8,198 @@ import sys
 import os
 
 
-def init(db_name=":memory:"):
-    """Initialize SQLite3 db-connection.
-    If the database file doesn't exist, it'll be created.
-    Makes use of Row-object to access through column-names (and indexing).
-    For a temporary database in memory, use ":memory:".
+class Tamer(sqlite3.Connection):
+    """Instanciated as a subclass of SQLite-connection object"""
+    def __init__(self, db_name=":memory:"):
+        """Initialize SQLite3 db-connection.
+        If the database file doesn't exist, it'll be created.
+        Makes use of Row-object to access through column-names (and indexing).
+        For a temporary database in memory, use the default ':memory:' name.
 
-    Args:
-        db_name: string containing a database-name (could also be a path-like object)
+        Args:
+            db_name: string containing a database-name (could also be a path-like object)
 
-    Returns:
-        Connection-object to access the database if succeed, otherwise None
+        Returns:
+            instance of sqlite3.Connection to access the database if succeed, otherwise None
 
-    Reading:
-        https://docs.python.org/3/library/sqlite3.html
-    """
-    try:
-        con = sqlite3.connect(db_name)
-        con.row_factory = sqlite3.Row
-        return con
-    except sqlite3.DatabaseError as err:
-        print("Couldn't initialize connection to database: ", err, file=sys.stderr)
-        return None
+        Reading:
+            https://docs.python.org/3/library/sqlite3.html#connection-objects
+        """
+        super().__init__(db_name)
+        self.row_factory = sqlite3.Row
 
+    def create(self, table_name, *column_names):
+        """Create table with provided columns.
+        The table will be created only if it doesn't exist already.
+        Immediatley commits after succesful execution of statement.
 
-def create(con, table_name, *column_names):
-    """Create table with provided columns.
-    The table will be created only if it doesn't exist already.
-    Immediatley commits after succesful execution of statement.
+        Args:
+            table_name:     string containing a valid table-name
+            *column_names:  strings separated by commas (arbitrary argument list)
+                            Addtionally created columns:
+                                1) rowid:       primary key (implicitly created by SQLite3
+                                2) added:       date of insertion
+                                3) modified:    date of last modification
 
-    Args:
-        con:            SQLite3 Connection-object (return value of init())
-        table_name:     string containing a valid table-name
-        *column_names:  strings separated by commas (arbitrary argument list)
-                        Addtionally created columns:
-                            1) rowid:       primary key (implicitly created by SQLite3
-                            2) added:       date of insertion
-                            3) modified:    date of last modification
+        Returns:
+            boolean:    indicates success
 
-    Returns:
-        boolean:    indicates success
-
-    Reading:
-        https://sqlite.org/lang_createtable.html
-        https://docs.python.org/3/tutorial/controlflow.html#arbitrary-argument-lists
-    """
-    try:
-        with con:
-            con.execute("""CREATE TABLE IF NOT EXISTS {}({}, added, modified)"""\
-                        .format(table_name, ", ".join(column_names)))
-        return True
-    except sqlite3.DatabaseError as err:
-        print("Couldn't create table:", err, file=sys.stderr)
-        return False
+        Reading:
+            https://sqlite.org/lang_createtable.html
+            https://docs.python.org/3/tutorial/controlflow.html#arbitrary-argument-lists
+        """
+        try:
+            with self:
+                self.execute("""CREATE TABLE IF NOT EXISTS {}({}, added, modified)"""\
+                             .format(table_name, ", ".join(column_names)))
+            return True
+        except sqlite3.Error as err:
+            print("Couldn't create table:", err, file=sys.stderr)
+            return False
 
 
-def insert(con, table, **kwargs):
-    """Insert new row into database.
-    Commits after succesful execution.
+    def insert(self, table, **kwargs):
+        """Insert new row into database.
+        Commits after succesful execution.
 
-    Args:
-        con:        SQLite3 Connection-object (return value of init())
-        table:      string containing a valid table-name
-        **kwargs:   columnname=value separated by commas. Unintentionally provided values for
-                    'rowid', 'added' and 'modified' will be discarded.
+        Args:
+            con:        SQLite3 Connection-object (return value of init())
+            table:      string containing a valid table-name
+            **kwargs:   columnname=value separated by commas. Unintentionally provided values for
+                        'rowid', 'added' and 'modified' will be discarded.
 
-    Returns:
-        primary key of last inserted row or None if insertion failed
+        Returns:
+            primary key of last inserted row or None if insertion failed
 
-    Reading:
-        https://sqlite.org/lang_insert.html
-        https://docs.python.org/3/library/stdtypes.html#mapping-types-dict
-    """
-    if kwargs.get("rowid"):
-        kwargs.pop("rowid")  # discard provided value for rowid as it's system property
-    lastrowid = None
-    cols = ", ".join(kwargs.keys())
-    qmarks = ", ".join("?" for _ in kwargs)
-    values = tuple(kwargs.values())
-    try:
-        with con:
-            lastrowid = con.execute("""
-            INSERT INTO {}({}, added, modified) VALUES({}, CURRENT_DATE, CURRENT_DATE)"""\
-            .format(table, cols, qmarks), values).lastrowid
-    except sqlite3.DatabaseError as err:
-        print("Couldn't insert item:", err, file=sys.stderr)
-    return lastrowid
+        Reading:
+            https://sqlite.org/lang_insert.html
+            https://docs.python.org/3/library/stdtypes.html#mapping-types-dict
+        """
+        if kwargs.get("rowid"):
+            kwargs.pop("rowid")  # discard provided value for rowid as it's system property
+        lastrowid = None
+        cols = ", ".join(kwargs.keys())
+        qmarks = ", ".join("?" for _ in kwargs)
+        values = tuple(kwargs.values())
+        try:
+            with self:
+                lastrowid = self.execute("""
+                INSERT INTO {}({}, added, modified) VALUES({}, CURRENT_DATE, CURRENT_DATE)"""\
+                .format(table, cols, qmarks), values).lastrowid
+        except sqlite3.Error as err:
+            print("Couldn't insert item:", err, file=sys.stderr)
+        return lastrowid
 
+        
+    def select(self, table, logic="OR", **kwargs):
+        """Select entire row(s) from database.
+        Using only the mandatory arguments selects everything.
+        Always selects primary key ('rowid') too.
 
-def select(con, table, logic="OR", **kwargs):
-    """Select entire row(s) from database.
-    Using only the mandatory arguments selects everything.
-    Always selects primary key ('rowid') too.
+        Args:
+            con:        SQLite3 Connection-object (return value of init())
+            table:      string containing a valid table-name
+            logic:      logical operator in the WHERE clause. This simple function won't allow to
+                        mix logical operators. Provided without kwargs won't have any effect.
+            **kwargs:   narrow selection with column=value pair(s). If more pairs are specified,
+                        they're bound together with the provided logical operator in the WHERE
+                        clause of the query. The default 'OR' means any, 'AND' means all of the
+                        conditions in kwargs must be met. 'NOT' is only partially supported, it
+                        makes only sense with one kwarg, but the latter won't be verified.
 
-    Args:
-        con:        SQLite3 Connection-object (return value of init())
-        table:      string containing a valid table-name
-        logic:      logical operator in the WHERE clause. This simple function won't allow to mix
-                    logical operators. Provided without kwargs won't have any effect.
-        **kwargs:   narrow selection with column=value pair(s). If more pairs are specified, they're
-                    bound together with the provided logical operator in the WHERE clause of the
-                    query. The default 'OR' means any, 'AND' means all of the conditions in kwargs
-                    must be met. 'NOT' is only partially supported, it makes only sense with one
-                    kwarg, but the latter won't be verified.
+        Returns:
+            Cursor-object of resulting query (powered as row_factory) or
+            None (slite3.Error happened)
 
-    Returns:
-        Cursor-object of resulting query (powered as row_factory) or
-        None (DatabaseError happened)
-
-    Reading:
-        https://sqlite.org/lang_select.html
-        https://www.w3schools.com/sql/sql_and_or.asp
-    """
-    select_stmnt = """SELECT DISTINCT rowid, * FROM {}"""
-    try:
-        if kwargs:
-            select_stmnt += _stmnt("WHERE", logic, **kwargs)
-            return con.execute(select_stmnt.format(table), tuple(kwargs.values()))
-        return con.execute(select_stmnt.format(table))
-    except sqlite3.DatabaseError as err:
-        print("Couldn't select any item:", err, file=sys.stderr)
-        return None
-
-
-def delete(con, table, logic="OR", **kwargs):
-    """Delete row(s) from database.
-    Commits after succesful execution.
-
-    Args:
-        con:        SQLite3 Connection-object (return value of init())
-        table:      string containing a valid table-name
-        logic:      logical operator in the WHERE clause. This simple function won't allow to mix
-                    logical operators. Provided without kwargs won't have any effect.
-        **kwargs:   what to delete as column=value pair(s). If more pairs are specified, they're
-                    bound together with the provided logical operator in the WHERE clause of the
-                    query. The default 'OR' means any, 'AND' means all of the conditions in kwargs
-                    must be met. 'NOT' is only partially supported, it makes sense just with one
-                    kwarg, but the latter won't be verified.
-
-    Returns:
-        boolean:    indicates success
-
-    Reading:
-        https://sqlite.org/lang_delete.html
-    """
-    delete_stmnt = """DELETE FROM {}""" + _stmnt("WHERE", logic, **kwargs)
-    try:
-        with con:
-            con.execute(delete_stmnt.format(table), tuple(kwargs.values()))
-        return True
-    except sqlite3.DatabaseError as err:
-        print("Couldn't delete item:", err, file=sys.stderr)
-        return False
+        Reading:
+            https://sqlite.org/lang_select.html
+            https://www.w3schools.com/sql/sql_and_or.asp
+        """
+        select_stmnt = """SELECT DISTINCT rowid, * FROM {}"""
+        try:
+            if kwargs:
+                select_stmnt += _stmnt("WHERE", logic, **kwargs)
+                return self.execute(select_stmnt.format(table), tuple(kwargs.values()))
+            return self.execute(select_stmnt.format(table))
+        except sqlite3.Error as err:
+            print("Couldn't select any item:", err, file=sys.stderr)
+            return None
 
 
-def update(con, table, what, logic="OR", **where):
-    """Update row(s).
-    Commits after succesful execution.
+    def delete(self, table, logic="OR", **kwargs):
+        """Delete row(s) from database.
+        Commits after succesful execution.
 
-    Args:
-        con:        SQLite3 Connection-object (return value of init())
-        table:      string containing a valid table-name
-        what:       dictionary containing column=new_value pairs
-        logic:      logical operator in the WHERE clause. This simple function won't allow to mix
-                    logical operators. Provided without **where won't have any effect.
-        **where:    update criteria as column=value pair(s). If more pairs are specified, they're
-                    bound together with the provided logical operator in the WHERE clause of the
-                    query. The default 'OR' means any, 'AND' means all of the conditions in kwargs
-                    must be met. 'NOT' is only partially supported, it makes sense just with one
-                    kwarg, but the latter won't be verified. Unintentionally provided values for
-                    'rowid', 'added' and 'modified' will be discarded.
+        Args:
+            con:        SQLite3 Connection-object (return value of init())
+            table:      string containing a valid table-name
+            logic:      logical operator in the WHERE clause. This simple function won't allow to
+                        mix logical operators. Provided without kwargs has no sense.
+            **kwargs:   specify criteria with column=value pair(s). If more pairs are specified,
+                        they're bound together with the provided logical operator in the WHERE
+                        clause of the query. The default 'OR' means any, 'AND' means all of the
+                        conditions in kwargs must be met. 'NOT' is only partially supported, it
+                        makes only sense with one kwarg, but the latter won't be verified.
 
-    Returns:
-        boolean:    indicates success
+        Returns:
+            boolean:    indicates success
 
-    Reading:
-        https://sqlite.org/lang_update.html
-    """
-    # discard provided values for these columns
-    if what.get("rowid"):
-        what.pop("rowid")
-    if what.get("added"):
-        what.pop("added")
-    if what.get("modified"):
-        what.pop("modified")
+        Reading:
+            https://sqlite.org/lang_delete.html
+        """
+        delete_stmnt = """DELETE FROM {}""" + _stmnt("WHERE", logic, **kwargs)
+        try:
+            with self:
+                self.execute(delete_stmnt.format(table), tuple(kwargs.values()))
+            return True
+        except sqlite3.Error as err:
+            print("Couldn't delete item:", err, file=sys.stderr)
+            return False
 
-    update_stmnt = """ UPDATE {}""" + _stmnt("SET", ",", **what)
-    update_stmnt += """, modified = CURRENT_DATE""" + _stmnt("WHERE", logic, **where)
 
-    try:
-        with con:
-            con.execute(update_stmnt.format(table),
-                        tuple(list(what.values()) + list(where.values())))
-        return True
-    except sqlite3.DatabaseError as err:
-        print("Couldn't update item:", err, file=sys.stderr)
-        return False
+    def update(self, table, what, logic="OR", **where):
+        """Update row(s).
+        Commits after succesful execution.
+
+        Args:
+            con:        SQLite3 Connection-object (return value of init())
+            table:      string containing a valid table-name
+            what:       dictionary containing column=new_value pairs
+            logic:      logical operator in the WHERE clause. This simple function won't allow to
+                        mix logical operators.
+            **where:    update criteria as column=value pair(s). If more pairs are specified,
+                        they're bound together with the provided logical operator in the WHERE
+                        clause of the query. The default 'OR' means any, 'AND' means all of the
+                        conditions in kwargs must be met. 'NOT' is only partially supported, it
+                        makes only sense with one kwarg, but the latter won't be verified.
+                         Unintentionally provided values for 'rowid', 'added' and 'modified' will
+                         be discarded.
+
+        Returns:
+            boolean:    indicates success
+
+        Reading:
+            https://sqlite.org/lang_update.html
+        """
+        # discard provided values for these columns
+        if what.get("rowid"):
+            what.pop("rowid")
+        if what.get("added"):
+            what.pop("added")
+        if what.get("modified"):
+            what.pop("modified")
+
+        update_stmnt = """ UPDATE {}""" + _stmnt("SET", ",", **what)
+        update_stmnt += """, modified = CURRENT_DATE""" + _stmnt("WHERE", logic, **where)
+
+        try:
+            with self:
+                self.execute(update_stmnt.format(table),
+                            tuple(list(what.values()) + list(where.values())))
+            return True
+        except sqlite3.Error as err:
+            print("Couldn't update item:", err, file=sys.stderr)
+            return False
 
 
 def destroy(con, db_name):
--- test_tamer.py
+++ test_tamer.py
@@ -1,71 +1,68 @@
-# test-specific imports
 import unittest
-
-# project-specific imports
 import sqlite3
 import tamer
 
 class TamerTest(unittest.TestCase):
-    """Testing sqlite_tamer module's functions"""
+    """Testing sqlite-tamer Tamer() class"""
 
     def setUp(self):
-        self.con = tamer.init()
-        tamer.create(self.con, "movies", "title", "year", "viewed")
-        tamer.insert(self.con, "movies", title="Star Wars", year=1977, viewed=2012)
-        tamer.insert(self.con, "movies", title="The Matrix", year=1999, viewed=50)
-        tamer.insert(self.con, "movies", title="Avengers", year=2012, viewed=2012)
-        tamer.insert(self.con, "movies", title="The Lord of The Rings", year=2002, viewed=2)
-        tamer.insert(self.con, "movies", title="2012", year=2009, viewed=1)        
-        tamer.insert(self.con, "movies", title="2012", year=2012, viewed=2012)
+        self.conn = tamer.Tamer()
+        self.conn.create("movies", "title", "year", "watched")
+        self.conn.insert("movies", title="Star Wars", year=1977, watched=2012)
+        self.conn.insert("movies", title="The Matrix", year=1999, watched=50)
+        self.conn.insert("movies", title="Avengers", year=2012, watched=2012)
+        self.conn.insert("movies", title="The Lord of The Rings", year=2002, watched=2)
+        self.conn.insert("movies", title="2012", year=2009, watched=1)
+        self.conn.insert("movies", title="2012", year=2012, watched=2012)
 
     def test_init(self):
-        self.assertIsInstance(self.con, sqlite3.Connection)
+        self.assertIsInstance(self.conn, sqlite3.Connection)
 
-    def test_create(self):        
-        self.assertTrue(tamer.create(self.con, "movies", "title", "year", "viewed"),
-                        "Create table failed")
+    def test_create(self):
+        table = self.conn.execute("""SELECT name FROM sqlite_master WHERE type = 'table'""")
+        self.assertEqual(next(table)["name"], "movies", "Creating 'movies' failed")
 
     def test_insert(self):
-        tamer.create(self.con, "movies", "title", "year", "viewed")
-        self.assertIsNotNone(tamer.insert(self.con, "movies", title="Star Wars", year=1977, viewed=2012), "Insert row failed")
+        rows = self.conn.execute("""SELECT * FROM movies""")
+        self.assertEqual(len(tuple(rows)), 6, "Inserting 6 rows failed")
 
-    def test_select_all(self):        
-        self.assertEqual(len(tuple(tamer.select(self.con, "movies"))), 6, "Failed to fetch 6 rows")
+    def test_select_all(self):
+        self.assertEqual(len(tuple(self.conn.select("movies"))), 6, "Failed to fetch 6 rows")
 
     def test_select_rowid(self):
-        self.assertEqual(next(tamer.select(self.con, "movies", rowid=2))["title"], "The Matrix", "Failed to fetch The Matrix")
+        self.assertEqual(next(self.conn.select("movies", rowid=2))["title"], "The Matrix", "Failed to fetch The Matrix")
 
     def test_select_or(self):
-        self.assertEqual(len(tuple(tamer.select(self.con, "movies", title="2012", year=2012, viewed=2012))), 4, "Failed to fetch 4 rows")
+        self.assertEqual(len(tuple(self.conn.select("movies", title="2012", year=2012, watched=2012))), 4, "Failed to fetch 4 rows")
 
     def test_select_and(self):
-        self.assertEqual(len(tuple(tamer.select(self.con, "movies", "AND", title="2012", year=2012, viewed=2012))), 1, "Failed to fetch 1 row")
+        self.assertEqual(len(tuple(self.conn.select("movies", "AND", title="2012", year=2012, watched=2012))), 1, "Failed to fetch 1 row")
 
     def test_select_not(self):
-        self.assertEqual(len(tuple(tamer.select(self.con, "movies", "NOT", viewed=1))), 5, "Failed to fetch 5 rows")
+        self.assertEqual(len(tuple(self.conn.select("movies", "NOT", watched=1))), 5, "Failed to fetch 5 rows")
 
     def test_delete_1(self):
-        tamer.delete(self.con, "movies", "NOT", viewed=1)
-        self.assertEqual(len(tuple(tamer.select(self.con, "movies"))), 1, "Failed to delete 5 rows")
+        self.conn.delete("movies", "NOT", watched=1)
+        self.assertEqual(len(tuple(self.conn.select("movies"))), 1, "Failed to delete 5 rows")
 
     def test_delete_2(self):
-        tamer.delete(self.con, "movies", year=2012)
-        self.assertEqual(len(tuple(tamer.select(self.con, "movies"))), 4, "Failed to delete 2 rows")
+        self.conn.delete("movies", year=2012)
+        self.assertEqual(len(tuple(self.conn.select("movies"))), 4, "Failed to delete 2 rows")
 
     def test_update_1(self):
-        tamer.update(self.con, "movies", {"viewed": 2013}, title="Star Wars")
-        self.assertEqual(next(tamer.select(self.con, "movies", rowid=1))["viewed"], 2013, "Failed to watch Star Wars one more time :-(")
+        self.conn.update("movies", {"watched": 2013}, title="Star Wars")
+        self.assertEqual(next(self.conn.select("movies", rowid=1))["watched"], 2013, "Failed to watch Star Wars one more time :-(")
 
     def test_update_2(self):
-        tamer.update(self.con, "movies", {"title": "Star Wars: A new hope", "viewed": 2013}, logic="AND", title="Star Wars", year=1977)
-        self.assertEqual(next(tamer.select(self.con, "movies", rowid=1))["title"], "Star Wars: A new hope", "Failed to update to Star Wars: A new hope")
-    
+        self.conn.update("movies", {"title": "Star Wars: A new hope", "watched": 2013}, logic="AND", title="Star Wars", year=1977)
+        self.assertEqual(next(self.conn.select("movies", rowid=1))["title"], "Star Wars: A new hope", "Failed to update to Star Wars: A new hope")
+
     def test_update_3(self):
-        tamer.update(self.con, "movies", {"title": "Star Wars: A new hope", "viewed": 2013}, logic="AND", title="Star Wars", year=1978)
-        self.assertNotEqual(next(tamer.select(self.con, "movies", rowid=1))["title"], "Star Wars: A new hope", "Updated the wrong Star Wars movie")
+        self.conn.update("movies", {"title": "Star Wars: A new hope", "watched": 2013}, logic="AND", title="Star Wars", year=1978)
+        self.assertNotEqual(next(self.conn.select("movies", rowid=1))["title"], "Star Wars: A new hope", "Updated the wrong Star Wars movie")
 
     def tearDown(self):
-        self.con.close()
+        self.conn.close()
 
 if __name__ == "__main__":
     unittest.main()
