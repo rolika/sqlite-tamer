c294a621ac77019e7acccaa6e3bcdb920bc44093
Weisz Roland <weisz.roland@pohlen-dach.hu>
2022-03-30 19:53:04 +0200

    check attached databases

--- test_attach.py
+++ test_attach.py
@@ -9,23 +9,18 @@ class AttachTest(unittest.TestCase):
     def setUp(self) -> None:
         self._conn = tamer.Tamer.create_from_json("sql_create.json", "sql_default.json")
         # setup a contact
-        person_id = self._conn["kontakt"].insert("szemely", vezeteknev="Doe", keresztnev="John")
-        organisation_id = self._conn["kontakt"].insert("szervezet", rovidnev="Example Inc.")
-        contact_id = self._conn["kontakt"].insert("kontakt", szemelyazonosito=person_id, szervezetazonosito=organisation_id)
-        self._customer_id = self._conn["kontakt"].insert("vevo", kontaktazonosito=contact_id)
-        self._responsible_id = self._conn["kontakt"].insert("ajanlatkeszito", kontaktazonosito=contact_id)
-        # setup a project
-        project_id = self._conn["projekt"].insert("projekt", nev="Main Project", ev=22, szam=314)
-        part_id = self._conn["projekt"].insert("munkaresz", projektazonosito=project_id, megnevezes="Part of Project")
-        self._trait = self._conn["projekt"].insert("jelleg", munkareszazonosito=part_id, megnevezes="new project")
+        self._person_id = self._conn["person"].insert("person", lastname="Doe", firstname="John")
+        self._conn["person"].insert("person", lastname="Doe", firstname="Jane")
+        self._company_id = self._conn["company"].insert("company", shortname="Example", fullname="Example Inc.")
+        self._conn["contact"].insert("contact", person_id=self._person_id, company_id=self._company_id)
     
-    def test_project(self):
-        request_id = self._conn["projekt"].insert("megkereses", jelleg=self._trait, ajanlatkero=self._customer_id, temafelelos=self._responsible_id)
-        return self.assertEqual(request_id, 1)
-    
-    def test_illegal_insert(self):
-        self.assertFalse(self._conn["projekt"].insert("megkereses", jelleg=20, ajanlatkero=245, temafelelos=4))
-            
+    def test_contact(self):
+        contact = self._conn["contact"].execute("""
+        SELECT printf('%s/%s', shortname, firstname)
+        FROM contact, person, company
+        WHERE person_id = ? AND company_id = ?;
+        """, (self._person_id, self._company_id)).fetchone()[0]
+        return self.assertEqual("Example/John", contact)            
 
     def tearDown(self):
         for conn in self._conn.values():
